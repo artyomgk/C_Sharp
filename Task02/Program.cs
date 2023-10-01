9f5ffb409165fdee7d0cf03196fa37dc35066b5c
artyomgk <rrrva43@gmail.com>
2023-10-01 18:49:42 +0300

    ДЗ Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -10,9 +10,10 @@ Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 if (number1 > number2)
     Console.WriteLine($"Первое число ({number1}) большее, второе число ({number2}) меньшее");
-if (number1 < number2)
+else if (number1 < number2)
     Console.WriteLine($"Второе число ({number2}) большее, первое число ({number1}) меньшее");    
 else if (number1 == number2)
     Console.WriteLine("Числа равны");
