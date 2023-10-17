c310d32e86e572ecc17e8f1a73ddfb0c86db3112
artyomgk <rrrva43@gmail.com>
2023-10-17 22:38:39 +0300

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,23 @@
+﻿// 11. Напишите программу, которая принимает на вход
+// трёхзначное число и на удаляет вторую
+// цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+int numResult = firstDigit * 10 + lastDigit;
+// Console.WriteLine($"{number} -> {numResult}");
+
+//Создание метода
+int DeleteSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    return numResult = firstDigit * 10 + lastDigit;
+}
+
+int MidDigit = DeleteSecondDigit(number);
+Console.WriteLine($"{number} -> {MidDigit}");
