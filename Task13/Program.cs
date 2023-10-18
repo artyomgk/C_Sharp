84050e328d2ab01989b07647d9ddb87d249a2811
artyomgk <rrrva43@gmail.com>
2023-10-18 23:36:58 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// int ThirdDigit (int num)
+// {
+    
+// }
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ThirdDigit(int num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    return num % 10;
+} 
+
+
+int res = ThirdDigit(number);
+Console.WriteLine(res);
